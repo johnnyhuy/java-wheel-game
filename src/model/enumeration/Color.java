d1f89f276b7ad3392f38d0abef364a96b112fdf5
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-03-08 15:13:52 +1100

    Auto-correct.

--- src/model/enumeration/Color.java
+++ src/model/enumeration/Color.java
@@ -2,7 +2,7 @@ package model.enumeration;
 
 /**
  * <pre> Provided enum type for Further Programming representing the color of a Slot on the Gaming Wheel
- * <b>NOTE:</b> GREEN0 and GREEN00 are provided as dfferent colors to differentiate the 0 and 00 which
+ * <b>NOTE:</b> GREEN0 and GREEN00 are provided as different colors to differentiate the 0 and 00 which
  * are both numeric zero in our implementation</pre>
  *
  * @author Caspar Ryan
