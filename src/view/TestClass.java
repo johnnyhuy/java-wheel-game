27b91f7dbfcb4bf786e6fc62c91ea44bfc613472
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-03-08 15:13:41 +1100

    Added JUnit 5 testing.

---
+++ src/view/TestClass.java
@@ -0,0 +1,12 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestClass {
+    @Test
+    void firstTest() {
+        assertTrue(true);
+    }
+}
