dd783c4369f28e5e407a5800cf5f9fe3856b8310
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-03-08 14:55:25 +1100

    Formatted the bloody source files.

--- src/model/enumeration/BetType.java
+++ src/model/enumeration/BetType.java
@@ -6,43 +6,35 @@ import model.interfaces.Slot;
 /**
  * Provided enum type for Further Programming representing a type of Bet<br>
  * See inline comments for where you need to provide additional code
- * 
+ *
  * @author Caspar Ryan
- * 
  */
-public enum BetType
-{
-   RED
-   {
-      @Override
-      public void applyWinLoss(Player player, Slot winSlot)
-      {
-         // TODO implementation
-      }
-   },
-   BLACK
-   {
-      @Override
-      public void applyWinLoss(Player player, Slot winSlot)
-      {
-         // TODO implementation
-      }
-   },
-   ZEROS
-   {
-      @Override
-      public void applyWinLoss(Player player, Slot winSlot)
-      {
-         // TODO implementation
-      }
-   };
+public enum BetType {
+    RED {
+        @Override
+        public void applyWinLoss(Player player, Slot winSlot) {
+            // TODO implementation
+        }
+    },
+    BLACK {
+        @Override
+        public void applyWinLoss(Player player, Slot winSlot) {
+            // TODO implementation
+        }
+    },
+    ZEROS {
+        @Override
+        public void applyWinLoss(Player player, Slot winSlot) {
+            // TODO implementation
+        }
+    };
 
-   /**
-    * This method is to be overridden for each bet type<br>
-    * see assignment specification for betting odds and how win/loss is applied
-    * 
-    * @param player - the player to apply the win/loss points balance adjustment
-    * @param winSlot - the winning slot the ball landed on
-    */
-   public abstract void applyWinLoss(Player player, Slot winSlot);
+    /**
+     * This method is to be overridden for each bet type<br>
+     * see assignment specification for betting odds and how win/loss is applied
+     *
+     * @param player  - the player to apply the win/loss points balance adjustment
+     * @param winSlot - the winning slot the ball landed on
+     */
+    public abstract void applyWinLoss(Player player, Slot winSlot);
 }
