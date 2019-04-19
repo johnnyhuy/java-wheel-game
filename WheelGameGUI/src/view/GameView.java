a8f85998a635c302046eb848627c6ee71b3b900f
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-04-19 14:17:26 +1000

    Added wheel game wheel.

--- WheelGameGUI/src/view/GameView.java
+++ WheelGameGUI/src/view/GameView.java
@@ -3,6 +3,11 @@ package view;
 import view.component.ViewMenuBar;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.net.URL;
+import java.util.Objects;
 
 public class GameView extends View {
     public GameView() {
@@ -11,5 +16,29 @@ public class GameView extends View {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        URL location = getClass().getClassLoader().getResource("resources/images/Basic_roulette_wheel_1024x1024.png");
+        final ImageIcon icon = new ImageIcon(Objects.requireNonNull(location));
+        JLabel label = new JLabel();
+
+        BorderLayout layout = new BorderLayout();
+
+        JPanel wheelPanel = new JPanel();
+        wheelPanel.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                Icon newIcon = new ImageIcon(icon.getImage().getScaledInstance(wheelPanel.getWidth(), wheelPanel.getHeight(), Image.SCALE_DEFAULT));
+                label.setIcon(newIcon);
+            }
+        });
+
+        JPanel summaryPanel = new JPanel();
+        summaryPanel.setBackground(Color.darkGray);
+
+        wheelPanel.setLayout(layout);
+        wheelPanel.add(label, BorderLayout.CENTER);
+//        wheelPanel.add(summaryPanel, BorderLayout.LINE_END);
+
+        add(wheelPanel);
     }
 }
