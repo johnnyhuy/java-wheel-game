903bbd0a2083ee0bebc94a169de85277a359688d
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-05-19 11:58:51 +1000

    Refactored a long awaited game view.

--- WheelGameGUI/src/view/GameView.java
+++ WheelGameGUI/src/view/GameView.java
@@ -3,58 +3,46 @@ package view;
 import controller.GameController;
 import controller.PlayerController;
 import model.interfaces.GameEngine;
-import model.interfaces.Player;
+import view.component.Updatable;
 import view.component.frame.GameFrame;
 import view.component.panel.SummaryPanel;
 import view.component.panel.ToolbarPanel;
 import view.component.panel.WheelPanel;
-import view.component.table.TableModel;
-import view.component.table.TableRenderer;
 import view.listener.game.GameFrameListener;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-
-import static helper.CollectionHelper.toList;
-import static helper.StringHelper.capitalize;
 
 public class GameView extends SubscriptionView {
+    private ArrayList<Updatable> updatables;
     private GameEngine gameEngine;
     private GameController gameController;
     private PlayerController playerController;
-    private DefaultTableModel dtm;
-    private SummaryPanel summaryPanel;
-    private JTable table;
-    private JScrollPane scrollPane;
-    private GameFrame frame;
-    private List<Player> players;
-    private ToolbarPanel toolbar;
 
     public GameView(GameEngine gameEngine, GameController gameController, PlayerController playerController) {
         this.gameEngine = gameEngine;
         this.gameController = gameController;
         this.playerController = playerController;
-        this.players = toList(gameEngine.getAllPlayers());
+        this.updatables = new ArrayList<>();
     }
 
     @Override
     public void render() {
-        // TODO: set minimum frame size
-        frame = new GameFrame(this, playerController);
+        GameFrame frame = new GameFrame(this, playerController);
         final int padding = 5;
 
-        toolbar = new ToolbarPanel(gameController, gameEngine, padding);
+        ToolbarPanel toolbar = new ToolbarPanel(gameController, gameEngine, padding);
+        updatables.add(toolbar);
         frame.add(toolbar, BorderLayout.NORTH);
 
         WheelPanel wheelPanel = new WheelPanel(gameEngine, padding * 10);
         frame.add(wheelPanel);
 
-        summaryPanel = new SummaryPanel(frame, wheelPanel);
-        paintSummaryPanel();
+        SummaryPanel summaryPanel = new SummaryPanel(gameEngine, frame, wheelPanel);
+        updatables.add(summaryPanel);
         frame.add(summaryPanel, BorderLayout.EAST);
 
         JPanel statusBar = new JPanel();
@@ -75,50 +63,11 @@ public class GameView extends SubscriptionView {
         gameEngine.addGameEngineCallback(new GameEngineCallbackGUI(gameController, gameEngine, this, wheelPanel));
     }
 
-    private void paintSummaryPanel() {
-        DefaultTableModel dtm = new TableModel();
-        dtm.addColumn("Name");
-        dtm.addColumn("Points");
-        dtm.addColumn("Bet");
-
-        for (Player player : players) {
-            String playerBet = "";
-            if (player.getBetType() != null) {
-                playerBet = player.getBet() + " on " + capitalize(player.getBetType().name());
-            }
-
-            Object[] row = new Object[]{
-                player.getPlayerName(),
-                player.getPoints(),
-                playerBet
-            };
-
-            dtm.addRow(row);
-        }
-
-        table = new JTable();
-        table.setDefaultRenderer(Object.class, new TableRenderer());
-        table.getTableHeader().setPreferredSize(new Dimension(0, 40));
-        table.setModel(dtm);
-        table.setRowHeight(40);
-
-        scrollPane = new JScrollPane();
-        scrollPane.setViewportView(table);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        scrollPane.setBackground(null);
-        summaryPanel.add(scrollPane, BorderLayout.CENTER);
-    }
-
     @Override
     public void onNext(Boolean success) {
-        players = toList(gameEngine.getAllPlayers());
-
-        toolbar.update();
-
-        summaryPanel.removeAll();
-        paintSummaryPanel();
-        summaryPanel.revalidate();
-        summaryPanel.repaint();
+        for (Updatable updatable : updatables) {
+            updatable.update();
+        }
 
         getSubscription().request(1);
     }
--- WheelGameGUI/src/view/component/panel/SummaryPanel.java
+++ WheelGameGUI/src/view/component/panel/SummaryPanel.java
@@ -1,24 +1,77 @@
 package view.component.panel;
 
+import model.interfaces.GameEngine;
+import model.interfaces.Player;
+import view.component.Updatable;
 import view.component.frame.GameFrame;
+import view.component.table.TableModel;
+import view.component.table.TableRenderer;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
-public class SummaryPanel extends JPanel {
+import static helper.StringHelper.capitalize;
+
+public class SummaryPanel extends JPanel implements Updatable {
+    private GameEngine gameEngine;
     private GameFrame frame;
     private WheelPanel wheelPanel;
 
-    public SummaryPanel(GameFrame frame, WheelPanel wheelPanel) {
+    public SummaryPanel(GameEngine gameEngine, GameFrame frame, WheelPanel wheelPanel) {
+        this.gameEngine = gameEngine;
         this.frame = frame;
         this.wheelPanel = wheelPanel;
 
         setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10), BorderFactory.createMatteBorder(1, 1, 1, 1, Color.LIGHT_GRAY)));
         setSize();
         setLayout(new BorderLayout());
+        paintTable();
+    }
+
+    private void paintTable() {
+        DefaultTableModel dtm = new TableModel();
+        dtm.addColumn("Name");
+        dtm.addColumn("Points");
+        dtm.addColumn("Bet");
+
+        for (Player player : gameEngine.getAllPlayers()) {
+            String playerBet = "";
+            if (player.getBetType() != null) {
+                playerBet = player.getBet() + " on " + capitalize(player.getBetType().name());
+            }
+
+            Object[] row = new Object[]{
+                player.getPlayerName(),
+                player.getPoints(),
+                playerBet
+            };
+
+            dtm.addRow(row);
+        }
+
+        JTable table = new JTable();
+        table.setDefaultRenderer(Object.class, new TableRenderer());
+        table.getTableHeader().setPreferredSize(new Dimension(0, 40));
+        table.setModel(dtm);
+        table.setRowHeight(40);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(table);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        scrollPane.setBackground(null);
+        add(scrollPane, BorderLayout.CENTER);
     }
 
     public void setSize() {
         setPreferredSize(new Dimension(frame.getWidth() / 3, wheelPanel.getHeight()));
+    }
+
+    @Override
+    public void update() {
+        removeAll();
+        paintTable();
+        revalidate();
+        repaint();
     }
 }
