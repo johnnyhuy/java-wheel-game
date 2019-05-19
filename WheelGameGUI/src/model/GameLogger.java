8e1eb72c791a10355fce820e7b629f02c3989581
Johnny Huynh <johnny.huynh@enett.com>
2019-05-19 21:33:12 +1000

    Updated doco.

--- WheelGameGUI/src/model/GameLogger.java
+++ WheelGameGUI/src/model/GameLogger.java
@@ -14,14 +14,31 @@ public class GameLogger {
         this.textPane = textPane;
     }
 
+    /**
+     * Log something to the text pane.
+     *
+     * @param text string
+     */
     public void log(String text) {
         print(text, Color.BLACK);
     }
 
+    /**
+     * Log something to the text pane with a specific color.
+     *
+     * @param text  string
+     * @param color color to set the text
+     */
     public void log(String text, Color color) {
         print(text, color);
     }
 
+    /**
+     * Print text in the text pane.
+     *
+     * @param text to print
+     * @param color on the text
+     */
     private void print(String text, Color color) {
         StyleContext styleContext = StyleContext.getDefaultStyleContext();
         AttributeSet attribute = styleContext.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, color);
